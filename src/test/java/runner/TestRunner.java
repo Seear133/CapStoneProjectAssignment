c2129b11b5287d7c3d50fc0d0cfa610a6cb79d7a
ahmad habibi <ahmadseearhabibi@gmail.com>
2021-03-29 16:45:28 -0400

    My CapStone Project

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features ="classpath:Features", // Features tells cucumber options where feature files are stored
 		glue = {"stepDefinitions"}, // glue tells cucumber options where all Step Definitions class are stored 
-		tags = {"@login"}, // we can run all scenarios or one scenario with help of tags 
+		tags = {"@TestEditingAccountCapStone"}, // we can run all scenarios or one scenario with help of tags 
 		dryRun = false, // dryRun sets false will check if all steps in scenario has step d
 		monochrome = true, // if set to true, makes console output readable 
 		strict =false, // if set to false will fail the execution if there are any pending or undefined steps
@@ -19,7 +19,7 @@ import cucumber.api.junit.Cucumber;
 		        "rerun:target/cucumber-reports/rerun.txt"},
 		plugin = "json:target/cucumber-reports/CucumberTestReport.json"
 		)
-public class TestRunner {
-
-	//test00000
+public class TestRunner { 
+ 
+	
 }
